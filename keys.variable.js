efe6132fb5d6c7e3a86427d731f9646ab7b3ee84
nicolas-dominguez <nidominguez@fi.uba.ar>
2023-10-12 12:38:16 -0300

    Inicio

--- keys.variable.js
+++ keys.variable.js
@@ -1,8 +1,9 @@
 //add keys
 
 //mongodb atlas database password
-export var mongodb = "";
+export var mongodb =
+  "mongodb+srv://nicodominguez1468:nicodb2023@cluster0.oyw1bco.mongodb.net/?retryWrites=true&w=majority";
 
 //Github keys
-export var clientID = "";
-export var clientSecret = "";
+export var clientID = "3ca934b4a8f476b9933a";
+export var clientSecret = "9835fa4dc516e3bb81358314c47c940d1f9ef496";
